38a8a36e5452f2d88dc584eaa9bf1e51ef6340f9
Junseop <gaon0403@gmail.com>
2019-12-10 21:35:33 +0900

    Implement simple reply bot

--- backend/slack_bot.py
+++ backend/slack_bot.py
@@ -1,20 +1,26 @@
 # TODO: https://blog.wky.kr/16
 import slack
 
-from token import TOKEN
+from config import TOKEN
 
 
-def response(client):
-    res = client.chat_postMessage(channel='#money', text='Hello world!')
+@slack.RTMClient.run_on(event='message')
+def msg_handler(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
 
-    assert res["ok"]
-    assert res["message"]["text"] == "Hello world!"
-    return res
+    if 'Hello' in data.get('text', []):
+        channel_id = data['channel']
+        thread_ts = data['ts']
+
+        user = data['user']
+        web_client.chat_postMessage(channel=channel_id,
+                                    text=f'Hi <@{user}>!', thread_ts=thread_ts)
 
 
 def __main():
-    client = slack.WebClient(token=TOKEN)
-    response(client)
+    rtm_client = slack.RTMClient(token=TOKEN)
+    rtm_client.start()
 
 
 if __name__ == '__main__':
